f4bbdd4e8658c4f6d50be9e7d174b2bd5b4c3fca
punky <cicmurox@gmail.com>
2024-02-05 22:16:10 +0530

    make add to cart work

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,6 +1,8 @@
 
 let productsHtml = '';
 
+//console.log(products);
+
 products.forEach((products)=>{
   productsHtml += `
   <div class="product-container">
@@ -22,7 +24,7 @@ products.forEach((products)=>{
   </div>
 
   <div class="product-price">
-    ${(products.price / 100).toFixed(2)}
+    ${(products.priceCents / 100).toFixed(2)}
   </div>
 
   <div class="product-quantity-container">
@@ -47,14 +49,38 @@ products.forEach((products)=>{
     Added
   </div>
 
-  <button class="add-to-cart-button button-primary">
+  <button class="add-to-cart-button button-primary js-add-to-cart"
+  data-product-id="${products.id}">
     Add to Cart
   </button>
 </div>
   `;
 });
 
-console.log(productsHtml);
-
 document.querySelector('.js-products-grid').innerHTML = productsHtml;
+
+document.querySelectorAll('.js-add-to-cart').forEach((button) => {
+  button.addEventListener('click',()=>{
+    const productId = button.dataset.productId;
+    let match;
+
+    cart.forEach((item)=>{
+      if(productId === item.productId){
+        match = item;
+      }
+    });
+
+    if(match){
+      match.quantity++;
+    }else{
+      cart.push({
+        productId,
+        quantity: 1
+      })
+    }
+    console.log(cart); 
+
+  });
+  
+});
 
