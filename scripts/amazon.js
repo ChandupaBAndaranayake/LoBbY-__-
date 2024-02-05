c66306ece0a4688c24c60b1478bc918c955b459f
punky <cicmurox@gmail.com>
2024-02-05 16:55:13 +0530

    used products.js files

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,29 +1,3 @@
-const products = [
-{
-  image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-  name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-  rating: {
-    stars : 4.5,
-    count: 87
-  },
-  price: 1090 //in cents 1$ = 100 cents
-},{
-  image: 'images/products/intermediate-composite-basketball.jpg',
-  name: 'Intermediate Size Basketball',
-  rating: {
-    stars : 4,
-    count: 127
-  },
-  price: 2095 //in cents 1$ = 100 cents
-},{
-  image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-  name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-  rating: {
-    stars : 4.5,
-    count: 56
-  },
-  price: 799 //in cents 1$ = 100 cents
-}];
 
 let productsHtml = '';
 
