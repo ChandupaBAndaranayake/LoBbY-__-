811b2caae3d502c353b1386bbf34248db43e445e
punky <cicmurox@gmail.com>
2024-02-07 02:21:10 +0530

    gg

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,12 @@
 import {cart, removeFromCart} from '../data/carts.js';
 import { products } from '../data/products.js';
-import { formatCurrency } from './utlis/money.js';
+import formatCurrency  from './utlis/money.js';
+import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
+import { deliverOptions } from '../data/deliveryOptions.js';
+
+const today = dayjs();
+const deliveryDate = today.add(7, 'days');
+console.log(deliveryDate.format('dddd, MMMM D'));
 
 let cartSummaryHtml = '';
 
@@ -48,51 +54,50 @@ cart.forEach((cartItem) => {
           <div class="delivery-options-title">
             Choose a delivery option:
           </div>
-          <div class="delivery-option">
-            <input type="radio" checked
-              class="delivery-option-input"
-              name="delivery-option-${matchProduct.id}">
-            <div>
-              <div class="delivery-option-date">
-                Tuesday, June 21
-              </div>
-              <div class="delivery-option-price">
-                FREE Shipping
-              </div>
-            </div>
-          </div>
-          <div class="delivery-option">
-            <input type="radio"
-              class="delivery-option-input"
-              name="delivery-option-${matchProduct.id}">
-            <div>
-              <div class="delivery-option-date">
-                Wednesday, June 15
-              </div>
-              <div class="delivery-option-price">
-                $4.99 - Shipping
-              </div>
-            </div>
-          </div>
-          <div class="delivery-option">
-            <input type="radio"
-              class="delivery-option-input"
-              name="delivery-option-${matchProduct.id}">
-            <div>
-              <div class="delivery-option-date">
-                Monday, June 13
-              </div>
-              <div class="delivery-option-price">
-                $9.99 - Shipping
-              </div>
-            </div>
-          </div>
+          ${deliveryOptionsHtml(matchProduct)}
         </div>
       </div>
     </div>
   `;
 });
 
+
+function deliveryOptionsHtml(matchProduct){
+  let html = '';
+
+  deliveryOptionsHtml.forEach((deliveryOption) =>{
+    const today = dayjs();
+    const deliveryDate = today.add(
+      deliverOptions.deliveryDays,
+      'days'
+    );
+
+    const dateString = deliveryDate.format(
+      'dddd, MMMM D'
+    );
+
+    const priceString = deliveryOption.priceCents === 0 
+      ? 'FREE'
+      : `$${formatCurrency(deliveryOption.priceCents)} -`;
+
+    html += `
+      <div class="delivery-option">
+        <input type="radio"
+          class="delivery-option-input"
+          name="delivery-option-${matchProduct.id}">
+        <div>
+          <div class="delivery-option-date">
+            ${dateString}
+          </div>
+          <div class="delivery-option-price">
+            $${priceString} - Shipping
+          </div>
+        </div>
+      </div>
+    `
+  });
+  return html;
+}
 
 document.querySelector('.js-order-summery').innerHTML = cartSummaryHtml;
 
