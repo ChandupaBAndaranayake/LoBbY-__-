58733bf03eddff5f7397986187ec7fd1b11e8dc4
punky <cicmurox@gmail.com>
2024-02-08 18:57:34 +0530

    555

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -16,7 +16,7 @@ export function renderOrderSummary(){
 
     const deliveryOptionId = cartItem.deliveryOptionId;
     const deliveryOption = getDeliveryOption(deliveryOptionId);
-   // console.log(deliveryOption);
+    
     const today = dayjs();
     const deliveryDate = today.add(deliveryOption.deliveryDays,'days');
     const dateString = deliveryDate.format('dddd, MMMM D');
